1d78e8010c199625d793d8069097bf128442530a
Mehroj-SK <115388393+Mehroj-SK@users.noreply.github.com>
2022-12-03 02:27:10 +0530

    Add files via upload
    tuples Question no : 3

---
+++ tupleQ3.py
@@ -0,0 +1,28 @@
+#write a program to unpack following tuple into 4 variable 
+#tuple1 = (10,20,30,40)
+
+# tuple1 = (10,20,30,40)
+# (P,Q,R,S) = tuple1
+# print(P)
+# print(Q)
+# print(R)
+# print(S) 
+
+#we call () as paranthesis
+
+
+#same question as for loop
+# tuple1 = (10,20,30,40)
+# for i in tuple1:
+#     print(i)
+
+#same question as while loop
+# tuple1 = (10,20,30,40)
+# i = 0
+# while i < len(tuple1):
+#     print(tuple1[i])
+#     i+=1
+
+
+tuple1 = (10,20,30,40)
+print(tuple1[1:3])
